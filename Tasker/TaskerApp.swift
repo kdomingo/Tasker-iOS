1808b0dda061aacaa936035c0fd9b72242edab1b
Kyle Emmanuel <ke.domingz@gmail.com>
2024-03-13 13:28:59 +0800

    Initial Commit

---
+++ Tasker/TaskerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskerApp.swift
+//  Tasker
+//
+//  Created by Kyle Emmanuel Domingo on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
